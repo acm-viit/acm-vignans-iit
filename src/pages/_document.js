16e28a47ea8f65e502552465eef7421e57dd165d
Gautam Ankoji <gautamankoji@gmail.com>
2024-06-03 17:24:44 +0530

    update open graph tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
-import siteMetadata from "@/utils/siteMetaData";
+// import siteMetadata from "@/utils/siteMetaData";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content={siteMetadata.description} />
+        {/* <meta name="description" content={siteMetadata.description} />
         <meta property="og:title" content={siteMetadata.title} />
         <meta property="og:description" content={siteMetadata.description} />
         <meta property="og:url" content={siteMetadata.siteUrl} />
@@ -18,7 +18,7 @@ export default function Document() {
         <meta name="googlebot" content="index, follow, noimageindex, max-video-preview:-1, max-image-preview:large, max-snippet:-1" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={siteMetadata.title} />
-        <meta name="twitter:image" content={siteMetadata.socialBanner} />
+        <meta name="twitter:image" content={siteMetadata.socialBanner} /> */}
         <link
             href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css"
             rel="stylesheet"
